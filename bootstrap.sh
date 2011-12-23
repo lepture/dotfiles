6bb81e539f13dbcd917cd6def596456f89d499b2
lepture <lepture@me.com>
2011-12-23 22:07:38 +0800

    changes in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,27 +8,35 @@ if [ ! -f ./vimrc ]; then
 fi
 
 link() {
-    ln -s "`pwd`/$1" "$HOME/.$1"
+    if [ ! -h $HOME/.$1 ]; then
+       ln -s "`pwd`/$1" "$HOME/.$1"
+    fi
 }
 
-echo "make hide directory"
-mkdir "$HOME/.hide"
+if [ ! -d $HOME/.hide ]; then
+    echo "make hide directory"
+    mkdir "$HOME/.hide"
+fi
 
-echo "init vim"
+echo "init vim ...."
 link vim
 link vimrc
 
-echo "init emacs"
-link emacs.d
+echo "init emacs ...."
+#link emacs.d
 
-echo "init git"
+echo "init git ...."
 link gitconfig
 
-echo "init zsh"
+echo "init zsh ...."
 link zshrc
 
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
-echo "chsh -s which zsh"
+if which zsh > /dev/null; then
+    echo "chsh -s `which zsh`"
+else
+    echo "install zsh"
+fi

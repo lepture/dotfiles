df8641ed3249b4a910ff1d31905c4bdf9f06cd00
Hsiaoming Yang <me@lepture.com>
2015-03-18 11:15:25 +0800

    Save fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,18 +8,26 @@ set -x PATH /usr/local/bin $PATH $HOME/.config/bin $HOME/.rbenv/bin
 set -x PYTHONSTARTUP $HOME/.config/pystartup.py
 
 # pyenv
+set -x PYTHON_BUILD_SKIP_MIRROR "1"
 status --is-interactive; and . (pyenv init -|psub)
+. (pyenv virtualenv-init -|psub)
+# set -x MACOSX_DEPLOYMENT_TARGET "10.6"
+set -x PYTHON_CONFIGURE_OPTS "--enable-universalsdk=/ --with-universal-archs=intel"
 
 # nodejs
 set -x NODE_PATH /usr/local/lib/node_modules
 
 # golang
 set -x GOPATH $HOME/workspace/golang
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $GOPATH/bin $PATH
 
 # rbenv
 . (rbenv init -|psub)
 
+# homebrew cask
+set -x HOMEBREW_CASK_OPTS "--caskroom=~/.config/Caskroom"
+
+alias cnpm="npm --registry=http://r.cnpmjs.org"
 alias ia="open -a \"iA Writer\""
-alias subl="open -a \"Sublime Text 2\""
+alias subl="open -a \"Sublime Text\""
 alias nw="~/Applications/node-webkit.app/Contents/MacOS/node-webkit"
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,43 +1,24 @@
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
-
-set -g __fish_git_prompt_color_branch magenta bold
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate "*"
-set -g __fish_git_prompt_char_dirtystate "+"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "x"
-set -g __fish_git_prompt_char_cleanstate "v"
-
-set -g __fish_git_prompt_color_dirtystate blue
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green bold
-
 function fish_prompt --description 'Write out the prompt'
+  set -l last_status $status
+  set -l branch (git rev-parse --abbrev-ref HEAD ^/dev/null)
 
-	set -l last_status $status
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+  if test $branch
+    echo -n '|'
+    set_color yellow
+    echo -n $branch
+    set_color normal
+  end
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	set_color normal
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+  end
 
-	printf '%s ' (__fish_git_prompt)
+  set_color normal
 
-	if not test $last_status -eq 0
-	set_color $fish_color_error
-	end
-
-	echo -n '$ '
-
+  echo -n '$ '
 end
